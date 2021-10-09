ae24a544939c90331f6bb029860b05f88570349c
dat-adi <dat.adithya@gmail.com>
2021-10-09 10:14:11 +0530

    Updating properties for the Post struct
    Addition of better requirements for the properties.
    json, for client and browser displays.
    bson, for MongoDB usage.

--- main.go
+++ main.go
@@ -120,10 +120,10 @@ func main() {
 */
 
 type Post struct {
-	ID       primitive.ObjectID `json:"id"`
-	Caption  string             `json:"caption"`
-	ImageURL string             `json:"image_url"`
-	PostedAt *time.Time         `json:"posted_at"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Caption  string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	ImageURL string             `json:"image_url,omitempty" bson:"image_url,omitempty"`
+	PostedAt *time.Time         `json:"posted_at, omitempty" bson:"posted_at,omitempty"`
 }
 
 type Posts []Post
