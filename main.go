dc6c450d24c514a3f852c30f2262f5ac0c2531c6
dat-adi <dat.adithya@gmail.com>
2021-10-09 11:07:42 +0530

    Reverted modularity changes

--- main.go
+++ main.go
@@ -38,3 +38,77 @@ func main() {
 	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/insta"))
 	handleRequests()
 }
+
+/*
+   From here, below this is the model/post.go
+*/
+
+type Post struct {
+	ID       primitive.ObjectID `json:"id"`
+	Caption  string             `json:"caption"`
+	ImageURL string             `json:"image_url"`
+	PostedAt *time.Time         `json:"posted_at"`
+}
+
+type Posts []Post
+
+func getPost(response http.ResponseWriter, request *http.Request) {
+	/*
+	   return type => (post *Post)
+	   parameter type => (id int64)
+	   Get one post by ID
+	*/
+	if request.Method == "GET" {
+		response.Header().Add("content-type", "application/json")
+		params := request.URL.Query().Get("id")
+		fmt.Fprintf(response, params)
+		id, _ := primitive.ObjectIDFromHex(params)
+		var post Post
+
+		collection := client.Database("insta").Collection("post")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		// BROKEN for some reason
+		err := collection.FindOne(ctx, User{ID: id}).Decode(&post)
+		if err != nil {
+			response.WriteHeader(http.StatusInternalServerError)
+			response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+			return
+		}
+		json.NewEncoder(response).Encode(post)
+	} else {
+		http.Redirect(response, request, "/", http.StatusFound)
+	}
+	fmt.Fprintf(response, "Endpoint Hit: Get Post by ID endpoint")
+}
+
+func postPost(response http.ResponseWriter, request *http.Request) {
+	/*
+	   parameter type => (post *Post)
+	   Create a post
+	*/
+	if request.Method == "POST" {
+		response.Header().Add("content-type", "application/json")
+		var post Post
+		json.NewDecoder(request.Body).Decode(&post)
+		collection := client.Database("insta").Collection("post")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		result, _ := collection.InsertOne(ctx, post)
+		json.NewEncoder(response).Encode(result)
+	} else {
+		http.Redirect(response, request, "/", http.StatusFound)
+	}
+
+	fmt.Fprintf(response, "Endpoint Hit: Create a Post endpoint")
+}
+
+func getAllPosts(response http.ResponseWriter, request *http.Request) {
+	/*
+	   parameter type => (user *User)
+	   return type => array of (post *Post)?
+	   Get all the posts made by a user
+	*/
+	if request.Method == "GET" {
+	} else {
+		http.Redirect(response, request, "/", http.StatusFound)
+	} fmt.Fprintf(response, "Endpoint Hit: Get all user Posts endpoint")
+}
